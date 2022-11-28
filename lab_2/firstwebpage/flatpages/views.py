043d4bf352aa930f5497c537673e623f9010cd42
yarssset <76969319+yarssset@users.noreply.github.com>
2022-11-28 21:54:14 +0300

    Add files via upload

---
+++ lab_2/firstwebpage/flatpages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from django import template
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'templates/static_handler.html')
+
